505f56ab6e4d2ad3ad69bc28124b3a53e1030cd0
Peter Zingg <peter@zingg.name>
2008-05-14 12:33:37 -0700

    reply to

--- app/models/family_mailer.rb
+++ app/models/family_mailer.rb
@@ -2,6 +2,7 @@ class FamilyMailer < ActionMailer::Base
   def reg_form_invite(fam)
     recipients  fam.send_to 
     from        APP_CONFIG[:mail_from]
+    reply_to    APP_CONFIG[:mail_reply_to] unless APP_CONFIG[:mail_reply_to].nil?
     subject     "Online Registration is Now Open"
     body        :family => fam
   end
